c63abb7ececf1ddbdc59405f09a25ec48ee1b88a
Jasmeet Kaur <mann.jazz96@gmail.com>
2018-09-26 12:25:02 +0530

    search

--- d.c
+++ d.c
@@ -1,43 +1,72 @@
-
-
-/*Search for an element from left and right using two different threads */
-
-
 #include<stdio.h>
 #include<stdlib.h>
 #include<pthread.h>
-long long sum =0;
-void *sum_runner(void *arg){
-  long long *limit_ptr = (long long*)arg;
-  long long limit = *limit_ptr;
+const int MAX = 100;
+int arr[100];
+int found = 0;
+int t;
+int n;
 
-  long long i =0;
-  for(i =0; i <= limit;i++){
-    sum+=i;
+void *search_l(void *arg){
 
+  int *s = (int *)arg;
+  int key = *s;
+  for(int i = 0; i < n;i++){
+    if(found == 0 && arr[i] == key){
+      t = 1;
+      found = 1;
+      break;
+    }
+    else if(found==1){
+      break;
+    }
+  }
+pthread_exit(0);
+}
+
+void *search_r(void *arg){
+
+  int *s = (int *)arg;
+  int key = *s;
+  for(int i = n-1; i >= 0;i--){
+    if(found == 0 && arr[i] == key){
+      t = 2;
+      found = 1;
+      break;
+    }
+    else if(found == 1){
+      break;
+    }
   }
   pthread_exit(0);
 }
 
-int main(int argc , char **argv){
-  if(argc < 2){
+int main(int argc, char ** argv){
+  if(argc<2 || argc > 101){
     exit(-1);
   }
-  long long limit ;
-  int num = argc -1;
-  pthread_t tid[num];
-  for(int i=0; i < num;i++){
-  limit = atoll(argv[i+1]);
-  pthread_attr_t  attr;
-  pthread_attr_init(&attr);
-  pthread_create(&tid[i],&attr,sum_runner,&limit);
+   n = argc-2;
 
-
-}
-  for(int i =0; i < num;i++){
-      pthread_join(tid[i],NULL);
+  for(int i =0; i < n;i++){
+    arr[i] = atoi(argv[i+1]);
   }
-
-  printf("%lld\n",sum );
-
+  int key = atoi(argv[n+1]);
+  pthread_t left;
+  pthread_t right;
+  pthread_create(&left,NULL,search_l,&key);
+  pthread_create(&right,NULL,search_r,&key);
+  pthread_join(left,NULL);
+  pthread_join(right,NULL);
+  if(found == 1){
+    printf("FOUND BY ");
+    if(t == 1){
+      printf("LEFT\n");
+    }
+    else{
+      printf("RIGHT\n");
+    }
+  }
+  else{
+    printf("NOT FOUND\n");
+  }
 }
